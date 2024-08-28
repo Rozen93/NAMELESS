65f5c511354a8b9aef384e777cf4b7052aeb65c9
Rozen <tigole_93@hotmail.com>
2024-08-28 07:21:31 -0500

    28C

--- src/components/container_rhino/Container.tsx
+++ src/components/container_rhino/Container.tsx
@@ -8,7 +8,7 @@ type containerProps = {
 
 function ContainerRhino({ children, isNormal, isFull }: containerProps) {
     return (
-        <section className={`${isNormal? 'p-3 lg:p-4 mt-14' : 'pt-3 lg:pt-4 mt-14'} ${isFull? 'py-3 px-0 lg:py-4 lg:px-0 mt-14' : 'pt-3 lg:pt-4 mt-14'}`}>
+        <section className={`${isNormal? 'p-3 lg:p-4 mt-14' : 'pt-3 lg:pt-4 mt-14'} ${isFull? 'py-3 px-0 lg:pt-4 lg:pb-14 lg:px-0 mt-14' : 'pt-3 lg:pt-4 mt-14'}`}>
             <div className={`${isNormal? 'container-rhino pt-9 mt-9' : 'container-full pt-9 mt-9'}`}>
                 {children}
             </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,31 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      fontFamily:{
-        nova: ['var(--font-nova)'],
-        roboto: ['var(--font-roboto)']
-      }
+      backgroundColor: {
+        primary: "#1E3A8A", // Azul oscuro
+        secondary: "#F43F5E", // Rojo rosado
+        accent: "#10B981", // Verde esmeralda
+        neutral: "#F5F5F5", // Gris claro
+        dark: "#111827", // Gris muy oscuro
+      },
+      fontFamily: {
+        nova: ["var(--font-nova)"],
+        roboto: ["var(--font-roboto)"],
+      },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
